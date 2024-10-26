48551faa0fecebfdbe39bc821d8c7acaed3ae017
KD-TrendOn <damirkrasilnicov2006@gmail.com>
2024-10-26 17:14:09 +0300

    добавлены яндекс эмбеддинги

--- app/config.py
+++ app/config.py
@@ -6,6 +6,11 @@ load_dotenv()
 class Settings:
     YANDEXGPT_IAM_TOKEN:str=os.getenv("YANDEXGPT_IAM_TOKEN")
     YANDEXGPT_MODEL_URI:str=os.getenv("YANDEXGPT_MODEL_URI")
+    YANDEXGPT_MAIN_MODEL_URI:str=os.getenv("YANDEX_MAIN_MODEL_URI")
+    EMBEDDINGS_MODEL:str=os.getenv("EMBEDDINGS_MODEL")
+    DEVICE:str=os.getenv("DEVICE")
+    PG_CONNECTION:str=os.getenv("PG_CONNETCTION")
+    PG_COLLECTION_NAME:str=os.getenv("PG_COLLECTION_NAME")
 
 
 settings = Settings()
